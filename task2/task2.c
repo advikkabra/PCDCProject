ead89610f3cf3995ce8391d75b30f8af2a87ca43
advik <advikkabra25@gmail.com>
2024-03-29 22:47:56 +0530

    rename files

--- task2/task2.c
+++ task2/task2.c
@@ -1,98 +1,27 @@
+#include "Task2.h"
+#include <errno.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+void print_prompt() { printf("db > "); }
 
-#define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
-typedef struct {
-    uint32_t id;
-    char username[COLUMN_USERNAME_SIZE + 1];
-    char email[COLUMN_EMAIL_SIZE + 1];
-} Row;
+// Struct defns given in Task2.h
+// Function declarations are also given in Task2.h
+// You need not use the same fxns, modify according to your implementation
+// and edit the declarations in .h later
+// You can add/delete things from .h file according to your code
 
-typedef struct {
-    StatementType type;
-    Row row_to_insert; // only used by insert statement
-} Statement;
+// add all fxns from task-1 here
 
-typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
+// add pager fxns with the functionalities given in Pager_template
 
-typedef enum {
-    PREPARE_SUCCESS,
-    PREPARE_NEGATIVE_ID,
-    PREPARE_STRING_TOO_LONG,
-    PREPARE_SYNTAX_ERROR,
-    PREPARE_UNRECOGNIZED_STATEMENT
-} PrepareResult;
+// add cursor fxns with the functionalities given in Cursor_template
 
-typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
-
-typedef struct {
-    char *buffer;
-    size_t buffer_length;
-    ssize_t input_length;
-} InputBuffer;
-
-typedef enum {
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult;
-
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
-
-extern const uint32_t ID_SIZE;
-extern const uint32_t USERNAME_SIZE;
-extern const uint32_t EMAIL_SIZE;
-extern const uint32_t ID_OFFSET;
-extern const uint32_t USERNAME_OFFSET;
-extern const uint32_t EMAIL_OFFSET;
-extern const uint32_t ROW_SIZE;
-
-#define TABLE_MAX_PAGES 100
-
-extern const uint32_t ROWS_PER_PAGE;
-extern const uint32_t TABLE_MAX_ROWS;
-
-void serialize_row(Row *source, void *destination);
-void deserialize_row(void *source, Row *destination);
-
-typedef struct {
-    int file_descriptor;
-    uint32_t file_length;
-    void *pages[TABLE_MAX_PAGES];
-} Pager;
-
-typedef struct {
-    uint32_t num_rows;
-    Pager *pager;
-} Table;
-
-void *get_page(Pager *pager, uint32_t page_num);
-
-typedef struct {
-    Table *table;
-    uint32_t row_num;
-    bool end_of_table; // Indicates a position one past the last element
-} Cursor;
-
-Cursor *table_start(Table *table);
-Cursor *table_end(Table *table);
-void *cursor_value(Cursor *cursor);
-void cursor_advance(Cursor *cursor);
-void pager_flush(Pager *pager, uint32_t page_num, uint32_t size);
-void db_close(Table *table);
-MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table);
-PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement);
-PrepareResult prepare_statement(InputBuffer *input_buffer,
-                                Statement *statement);
-Pager *pager_open(const char *filename);
-Table *db_open(const char *filename);
-void print_row(Row *row);
-ExecuteResult execute_insert(Statement *statement, Table *table);
-ExecuteResult execute_select(Statement *statement, Table *table);
-ExecuteResult execute_statement(Statement *statement, Table *table);
-InputBuffer *new_input_buffer();
-void read_input(InputBuffer *input_buffer);
-void close_input_buffer(InputBuffer *input_buffer);
+int main(int argc, char *argv[]) {
+}
